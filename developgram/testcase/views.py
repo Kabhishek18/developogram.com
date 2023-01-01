f2b0933c8322acecb3b0a46b5146a216a744de68
Abhishek <kumar.a.abhishek@capgemini.com>
2023-01-01 17:35:37 +0530

    Init: Testcase

--- developgram/testcase/views.py
+++ developgram/testcase/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from blogs.models import Post
 
-# Create your views here.
+def index(request):
+    # queryset = Post.objects.all()
+    queryset = Post.objects.all()
+    data = {
+            'querysets':queryset,
+            'title': 'Title Testing',
+            'content': 'content Testing',
+            'meta': 'Meta Testing',
+    }
+    return render(request, "testcase/index.html", data)
